719fd57bc29a71a4721e84e3ae2533c8f31f8809
CapisizuCosmin <cosmineala@gmail.com>
2020-11-27 16:00:05 +0200

    Playfair cipher only formatted text

---
+++ DS/DS/Playfair/Playfair.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS.Playfair
+{
+    public class Playfair
+    {
+    }
+}
--- DS/Program/Program.cs
+++ DS/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using DS.Caesar;
+using DS.Playfair;
 
 namespace Program
 {
@@ -10,7 +11,22 @@ namespace Program
         {
             Console.WriteLine("Hello World!");
 
-            Caesar.Decrypt("asd", 20);
+            //                COSTEL MERGE LA CUMPARATURI
+            string set1_form = "CO ST EL ME RG EL AC UM PA RA TU RI";
+            string set1_brut = "Costel merge la cumparaturi";
+
+            string key = "CORNEL";
+
+            string set2_form = "AN AX RE ME RE SI GU TU IX";
+            string set2_brut = " Ana are mere Si gutui ";
+
+            string encrypteed = "DO BW NC UF NC XS MP UP SR";
+
+            Console.WriteLine(
+                Playfair_cipher.Encipher( set2_brut , key) + "\n" + 
+                Playfair_cipher.Decipher( encrypteed, key )
+
+            );
         }
     }
 }
