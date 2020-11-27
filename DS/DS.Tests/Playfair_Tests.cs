39b061d28c436221d4bcc64156bfcac69ee7a44c
CapisizuCosmin <cosmineala@gmail.com>
2020-11-27 17:14:26 +0200

    Playfair cipher Full

---
+++ DS/DS.Tests/Playfair_Tests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DS.Playfair;
+
+namespace DS.Tests
+{
+    [TestClass]
+    public class Playfair_Tests
+    {
+        [TestMethod]
+        public void Encrypt_Returns_CorrectResault()
+        {
+            // Test formatted text
+            string input = "AN AX RE ME RE SI GU TU IX";
+            string key = "CORNEL";
+            string output = "";
+            string output_check = "DO BW NC UF NC XS MP UP SR";
+
+            output = Playfair.Playfair.Encrypt(input, key);
+
+            Assert.AreEqual(output_check, output);
+
+            // Test unformateed text
+            input = "Ana a re mere si gutu   i    ";
+            key = "CO RNe L";
+            output = "";
+            output_check = "DO BW NC UF NC XS MP UP SR";
+
+            output = Playfair.Playfair.Encrypt(input, key);
+
+            Assert.AreEqual(output_check, output);
+
+        }
+
+        [TestMethod]
+        public void Decrypt_Returns_CorrectResault()
+        {
+            // Test formatted text
+            string output_check = "AN AX RE ME RE SI GU TU IX";
+            string key = "CORNEL";
+            string output = "";
+            string input = "DO BW NC UF NC XS MP UP SR";
+
+            output = Playfair.Playfair.Decrypt(input, key);
+
+            Assert.AreEqual(output_check, output);
+
+            // Test unformateed text
+            input = " DobW NCUF NcxS MP UP SR";
+            key = "CO RNe L";
+            output = "";
+            output_check = "AN AX RE ME RE SI GU TU IX";
+
+            output = Playfair.Playfair.Decrypt(input, key);
+
+            Assert.AreEqual(output_check, output);
+        }
+
+    }
+}
--- DS/Program/Program.cs
+++ DS/Program/Program.cs
@@ -9,24 +9,9 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            //                COSTEL MERGE LA CUMPARATURI
-            string set1_form = "CO ST EL ME RG EL AC UM PA RA TU RI";
-            string set1_brut = "Costel merge la cumparaturi";
+            Console.WriteLine("Hello World");
 
-            string key = "CORNEL";
-
-            string set2_form = "AN AX RE ME RE SI GU TU IX";
-            string set2_brut = " Ana are mere Si gutui ";
-
-            string encrypteed = "DO BW NC UF NC XS MP UP SR";
-
-            Console.WriteLine(
-                Playfair_cipher.Encipher( set2_brut , key) + "\n" + 
-                Playfair_cipher.Decipher( encrypteed, key )
-
-            );
         }
     }
 }
