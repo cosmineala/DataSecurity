afb69c3396c41bce691814a0d4f9eefa7f39c4cd
CapisizuCosmin <cosmineala@gmail.com>
2020-12-08 03:05:02 +0200

    Create CombGen.cs

---
+++ DS/DS/Dictionary/CombGen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DS.Dictionary
+{
+    public class CombGen
+    {
+        public static string AlphabetLow { get; } = "abcdefghijklmnopqrstuvwxyz";
+        public static string AlphabetUp { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string Alphabet { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+
+        public IEnumerable<string> LastGenerate { get; set; }
+
+        public IEnumerable<string> Generate( int lenght, string alphabet)
+        {
+            var q = alphabet.Select(x => x.ToString());
+            for (int i = 0; i < lenght - 1; i++)
+            {
+                q = q.SelectMany(x => alphabet, (x, y) => x + y);
+            }
+            this.LastGenerate = q;
+            return q;
+        }
+    }
+}
