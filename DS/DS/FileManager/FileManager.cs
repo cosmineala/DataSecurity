e5d72072cb5bad329e983a47ba099d7109abde45
CapisizuCosmin <cosmineala@gmail.com>
2020-12-15 01:18:36 +0200

    Added FileManager

---
+++ DS/DS/FileManager/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DS.FileManager
+{
+    public class FileManager
+    {
+        static public string CurentDirectory { get; } = Directory.GetCurrentDirectory();
+        static public string InputsDirectory { get; } = FileManager.GetParent(CurentDirectory, 4) + "\\Inputs";
+       
+        static public string GetParent(string path) =>
+            System.IO.Directory.GetParent(path).ToString();
+        static public string GetParent(string path, int level)
+        {
+            string output = GetParent(path);
+            for(int i=0; i < level-1; i++)
+            {
+                output = GetParent(output);
+            }
+            return output;
+        }
+
+        static public string GetInputFile( string fileName)
+        {
+            return InputsDirectory + "\\" + fileName;
+        }
+
+        static public bool IsFile(string file) =>
+            File.Exists(file);
+
+        static public System.IO.FileStream CreateFile(string file) =>
+            File.Create(file);
+    }
+}
