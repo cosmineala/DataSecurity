1c0e94f8bc15ef84b55fb056ae48c7bde2bed5b5
CapisizuCosmin <cosmineala@gmail.com>
2021-01-16 22:45:12 +0200

    Beaufort example

--- DS/Program/Program.cs
+++ DS/Program/Program.cs
@@ -16,16 +16,19 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            // Fisierele se gasesc in folderul DS/Inputs ( nu in folderul DS care contine codul, in folderul DS care contine solutia si toate proiectele )
+            // Fisierele de intrare trebuie sa contina parola pe primul rand si testul pe urmatorul rand
+            // Fisierele de iesire au "--Beaufort...." dupa numele mostemnit de la fisierul original si au acceasi structura
 
-            var i1 = "PEOPL EOFME DIOCR EABIL ITYSO METIM ESACH IEVEO UTSTA NDING";
-            var i2 = "OEZEL KRIHD QAFRO ZIMLL GMPBT HEALT ZQNRP GBSPT ZPBAF QFFGQ";
+            var i1 = "PEOPL EOFME DIOCR EABIL ITYSO METIM ESACH IEVEO UTSTA NDING"; // text in clar
+            var i2 = "OEZEL KRIHD QAFRO ZIMLL GMPBT HEALT ZQNRP GBSPT ZPBAF QFFGQ"; // varianta criptata a textului in clar 
 
-            var k  = "DINTWOFNTHTITTF";
+            var k  = "DINTWOFNTHTITTF"; // key
 
-            Vigenere.Print.BeaufortEncrypt(i1, k);
+            Vigenere.Print.BeaufortEncrypt(i1, k); // print in consola
             Vigenere.Print.BeaufortDecrypt(i2, k);
 
-            Vigenere.File.BeaufortEncrypt("DeCriptat_1.txt");
+            Vigenere.File.BeaufortEncrypt("DeCriptat_1.txt"); // output de fisier
             Vigenere.File.BeaufortDecrypt("DeDecriptat_2.txt");
         }
     }
